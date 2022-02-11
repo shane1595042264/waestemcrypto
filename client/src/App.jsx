c9c0059d0fe635fc1003590f92630262e10bfdd9
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-02-11 13:49:32 -0500

    fancy admin authentication?

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+const  App=()=> {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+         <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
